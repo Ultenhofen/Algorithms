e3fa383dce9f5946383a42f493bbf940705057d8
Maximilian Altenhofen <48171863+Ultenhofen@users.noreply.github.com>
2020-12-01 11:31:26 -0800

    Update binarySearch2D.py

--- binarySearch2D.py
+++ binarySearch2D.py
@@ -34,8 +34,3 @@ def binarySearch(k , list):
         return BS(k, list, x, y)
     else: # Not in matrix
         return "Not Found"
-
-# Main
-list = [[1, 2, 3],[4, 5, 6],[7, 8, 9],[10,11,12]]
-
-print(binarySearch(1, list))
